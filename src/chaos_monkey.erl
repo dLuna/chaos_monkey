f7b2502a51bfa9e09b752ba5db2e1656e08aa7eb
Daniel Luna <daniel@lunas.se>
2012-07-06 21:05:05 -0400

    Start killing.

--- src/chaos_monkey.erl
+++ src/chaos_monkey.erl
@@ -8,6 +8,8 @@
 %% API
 -export([start_link/0]).
 
+-export([kill/0]).
+
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -19,15 +21,20 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+kill() ->
+    gen_server:call(?SERVER, kill, infinity).
+
 init([]) ->
     {ok, #state{}}.
 
-handle_call(_Request, _From, State) ->
+handle_call(kill, _From, State) ->
+    NewState = kill_something(State),
     Reply = ok,
-    {reply, Reply, State}.
+    {reply, Reply, NewState}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_cast(kill, State) ->
+    NewState = kill_something(State),
+    {noreply, NewState}.
 
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -37,3 +44,10 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+kill_something(State = #state{}) ->
+    Victim = hd(erlang:processes()),
+    exit(Victim, im_killing_you),
+    timer:sleep(500),
+    exit(Victim, kill),
+    State.
